46d7304479491ce6fcc4f5587f4d1f33afc4a1f1
kiwamizamurai <24860100+kiwamizamurai@users.noreply.github.com>
2022-03-15 19:21:09 +0900

    add nvim configuration

---
+++ packages/nvim/lua/lualine-config/init.lua
@@ -0,0 +1,3 @@
+require('lualine').setup({
+  options = {theme = 'tokyodark', section_separators = '', component_separators = ''}
+})
---
+++ packages/nvim/lua/toggleterm-config/init.lua
@@ -0,0 +1,11 @@
+require("toggleterm").setup{
+  size = 13,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  start_in_insert = true,
+  persist_size = true,
+  direction = 'horizontal',
+}
